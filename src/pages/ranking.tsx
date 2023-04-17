fcf47ee35d486894f6a1905ebf490d14588d672e
paulohfreire <paulo.freire53@gmail.com>
2023-04-17 19:36:04 -0300

    add styling to table and add teams

--- src/pages/ranking.tsx
+++ src/pages/ranking.tsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-
 type Props = {
   ids: number[];
 };
@@ -11,9 +10,9 @@ export default function Ranking({ ids }: Props) {
   useEffect(() => {
     const fetchData = async () => {
       const ids = [
-        7969983, 8025416, 8631482, 9035902, 1909960, 1229763, 1071737, 1147402,
-        28258296, 1614368, 13916753, 19653180, 47629775, 406869, 47646361,
-        47663847, 1941336, 47703776,
+        19653180, 13916753, 7969983, 1941336, 47663847, 47646361, 8631482,
+        9035902, 19298673, 47629775, 47996695, 1331986, 1276535, 26328842,
+        1071737, 47703776, 1909960, 406869, 47829504, 1614368,
       ];
       const responses = await Promise.all(
         ids.map((id) =>
@@ -28,22 +27,41 @@ export default function Ranking({ ids }: Props) {
   }, []);
 
   return (
-    <table id="team-scores">
+    <table
+      id="team-scores"
+      className="w-3/5 mx-auto min-h-screen border-collapse"
+    >
       <thead>
         <tr>
-          <th>Posição</th>
-          <th>Time</th>
-          <th>Pontuação Geral</th>
-          <th>Rodada Atual</th>
+          <th className="border px-4 py-2 text-center bg-gray-900 font-bold">
+            Time
+          </th>
+          <th className="border px-4 py-2 text-center bg-gray-900 font-bold">
+            Pontuação Geral
+          </th>
+          <th className="border px-4 py-2 text-center bg-gray-900 font-bold">
+            Rodada Atual
+          </th>
+          <th className="border px-4 py-2 text-center bg-gray-900 font-bold">
+            Patrimônio
+          </th>
         </tr>
       </thead>
       <tbody>
         {teamData.map((data, index) => (
           <tr key={index}>
-            <td>{data.ranking.atual.posicao}</td>
-            <td>{data.time.nome}</td>
-            <td>{data.pontos_campeonato}</td>
-            <td>{data.pontos}</td>
+            <td className="border px-4 py-2 text-center bg-gray-500 ">
+              {data.time.nome}
+            </td>
+            <td className="border px-4 py-2 text-center bg-gray-500 ">
+              {Number(data.pontos_campeonato).toFixed(2)}
+            </td>
+            <td className="border px-4 py-2 text-center bg-gray-500 ">
+              {Number(data.pontos).toFixed(2)}
+            </td>
+            <td className="border px-4 py-2 text-center bg-gray-500 ">
+              {data.patrimonio}
+            </td>
           </tr>
         ))}
       </tbody>
