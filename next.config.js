98ec06dfc9c6f9f1998a46ce694b2425ea2a2456
paulohfreire <paulo.freire53@gmail.com>
2023-05-12 11:39:01 -0300

    change headers to fix cors policy

--- next.config.js
+++ next.config.js
@@ -9,11 +9,11 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://cartolapp-6rlz9do8w-paulohfreire.vercel.app",
+            value: "https://cartolapp.vercel.app",
           },
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://api.cartola.globo.com",
+            value: "https://cartola.globo.com",
           },
           {
             key: "Access-Control-Allow-Methods",
