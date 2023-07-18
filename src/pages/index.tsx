a66edcf584b1afbaff152864a0d726ffa30767d1
paulohfreire <paulo.freire53@gmail.com>
2023-07-17 23:46:10 -0300

    added style and fit to small screens

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
+import { FaArrowRight } from "react-icons/fa";
 
 type Props = {
   ids: number[];
@@ -84,23 +85,37 @@ export default function Home({ ids }: Props) {
   // };
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      <table
-        id="team-scores"
-        className="w-3/5 mx-auto min-h-screen border-collapse"
-      >
-        <thead>
-          <tr>
-            <th className="border px-4 py-2 text-center bg-gray-900 font-bold">
-              Classificação
-            </th>
-            <th className="border px-4 py-2 text-center bg-gray-900 font-bold">
-              Time
-            </th>
-            <th className="border px-4 py-2 text-center bg-gray-900 font-bold">
-              <div className="flex items-center justify-between">
-                Pontuação Geral
-                {/* <select
+    <div className="flex flex-col items-center min-w-fit m-4 p-4 dark:bg-black">
+      <div className="w-full bg-black py-8 ">
+        <div className="w-full container p-2 flex justify-between items-center mx-auto py-8">
+          <h1 className="text-white text-center text-2xl  font-bold">
+            Liga Garoteantes
+          </h1>
+          <Link
+            href="/desafio"
+            className="text-white text-sm font-bold hover:text-gray-300"
+          >
+            <div className="flex-col">
+              <span>Bolas de ouro</span>
+              <FaArrowRight />
+            </div>
+          </Link>
+        </div>
+      </div>
+      <div className="container flex items-center m-2 py-2">
+        <table id="team-scores" className="w-full shadow rounded">
+          <thead>
+            <tr>
+              <th className="border px-1 py-1 text-center bg-gray-900 font-bold">
+                Pos.
+              </th>
+              <th className="border px-4 py-2 text-center bg-gray-900 font-bold">
+                Time
+              </th>
+              <th className="border px-4 py-2 text-center bg-gray-900 font-bold">
+                <div className="flex items-center justify-between">
+                  Pontuação Geral
+                  {/* <select
                   className="ml-1 p-1 bg-gray-900 cursor-pointer required:invalid"
                   onChange={handleSortByPontosCampeonato}
                   value={sortOrder}
@@ -108,15 +123,15 @@ export default function Home({ ids }: Props) {
                   <option value="desc">↑</option>
                   <option value="asc">↓</option>
                 </select> */}
-              </div>
-            </th>
-            <th className="border px-4 py-2 text-center bg-gray-900 font-bold">
-              Última Rodada
-            </th>
-            <th className="border px-4 py-2 text-center bg-gray-900 font-bold">
-              <div className="flex items-center justify-between">
-                Patrimônio
-                {/* <select
+                </div>
+              </th>
+              <th className="border px-4 py-2 text-center bg-gray-900 font-bold">
+                Última Rodada
+              </th>
+              <th className="border px-4 py-2 text-center bg-gray-900 font-bold">
+                <div className="flex items-center justify-between">
+                  Patrim.
+                  {/* <select
                   className="ml-1 p-1 bg-gray-900 rounded cursor-pointer required:invalid"
                   onChange={handleSortByPatrimonio}
                   value={sortOrder}
@@ -124,49 +139,47 @@ export default function Home({ ids }: Props) {
                   <option value="desc">↑</option>
                   <option value="asc">↓</option>
                 </select> */}
-              </div>
-            </th>
-            <th className="border px-4 py-2 text-center bg-gray-900 font-bold">
-              Média
-            </th>
-            <th className="border px-4 py-2 text-center bg-gray-900 font-bold">
-              Dif to lider
-            </th>
-
-            <th>
-              {" "}
-              <Link href="/desafio">Ir para Desafios bolas de ouro →</Link>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {teamData.map((data, index) => (
-            <tr key={index}>
-              <td className="border px-4 py-2 text-center bg-gray-500">
-                {index + 1}
-              </td>
-              <td className="border px-4 py-2 text-center bg-gray-500 ">
-                {data.time.nome}
-              </td>
-              <td className="border px-4 py-2 text-center bg-gray-500 ">
-                {Number(data.pontos_campeonato).toFixed(2)}
-              </td>
-              <td className="border px-4 py-2 text-center bg-gray-500 ">
-                {Number(data.pontos).toFixed(2)}
-              </td>
-              <td className="border px-4 py-2 text-center bg-gray-500 ">
-                {data.patrimonio}
-              </td>
-              <td className="border px-4 py-2 text-center bg-gray-500">
-                {Number(data.pontos_campeonato / data.rodada_atual).toFixed(2)}
-              </td>
-              <td className="border px-4 py-2 text-center bg-gray-500">
-                {Number(diffToLeaderData[index]).toFixed(2)}
-              </td>
+                </div>
+              </th>
+              <th className="border px-4 py-2 text-center bg-gray-900 font-bold">
+                Média
+              </th>
+              <th className="border px-4 py-2 text-center bg-gray-900 font-bold">
+                Dif to lider
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {teamData.map((data, index) => (
+              <tr key={index}>
+                <td className="border px-4 py-2 text-center bg-gray-500">
+                  {index + 1}
+                </td>
+                <td className="border px-4 py-2 text-center bg-gray-500 ">
+                  {data.time.nome}
+                </td>
+                <td className="border px-4 py-2 text-center bg-gray-500 ">
+                  {Number(data.pontos_campeonato).toFixed(2)}
+                </td>
+                <td className="border px-4 py-2 text-center bg-gray-500 ">
+                  {Number(data.pontos).toFixed(2)}
+                </td>
+                <td className="border px-4 py-2 text-center bg-gray-500 ">
+                  {data.patrimonio}
+                </td>
+                <td className="border px-4 py-2 text-center bg-gray-500">
+                  {Number(data.pontos_campeonato / data.rodada_atual).toFixed(
+                    2
+                  )}
+                </td>
+                <td className="border px-4 py-2 text-center bg-gray-500">
+                  {Number(diffToLeaderData[index]).toFixed(2)}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
